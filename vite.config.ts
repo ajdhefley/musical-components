30503325e78bdc5e4fd452843b89b4e972acc007
Ashton Hefley <ajdhefley@gmail.com>
2022-10-30 10:47:00 -0500

    Include declaration files in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import dts from "vite-plugin-dts"
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+
+        dts({
+            insertTypesEntry: true
+        }),
+    ],
     resolve: {
         alias: {
             '@lib' : resolve(__dirname, './src/lib')
