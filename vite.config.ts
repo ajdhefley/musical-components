c03e3392ec2a3d4d45ae2c9bbb8449e5cc0bc93d
Ashton Hefley <ajdhefley@gmail.com>
2022-10-30 09:42:37 -0500

    Build as library

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,35 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-        '@lib' : resolve(__dirname, './src/lib')
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@lib' : resolve(__dirname, './src/lib')
+        }
+    },
+    build: {
+        lib: {
+            // Could also be a dictionary or array of multiple entry points
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'musical-components',
+            // the proper extensions will be added
+            fileName: 'main'
+        },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ['vue'],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    vue: 'Vue'
+                }
+            }
+        },
+        outDir: 'build'
+    },
+    define: {
+        'process.env': process.env
     }
-  },
-  build: {
-    outDir: 'build'
-  },
-  define: {
-    'process.env': process.env
-  }
 })
