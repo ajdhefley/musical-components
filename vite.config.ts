49ffcb9338f4ad30ff4f139de566ec28f432d8e1
Ashton Hefley <ajdhefley@gmail.com>
2023-03-01 15:54:00 -0600

    lib - bundle css with exported js

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import dts from "vite-plugin-dts"
+import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
+
+        cssInjectedByJsPlugin(),
 
         dts({
             insertTypesEntry: true
@@ -18,22 +21,18 @@ export default defineConfig({
         }
     },
     build: {
+        cssCodeSplit: true,
         lib: {
-            // Could also be a dictionary or array of multiple entry points
             entry: resolve(__dirname, 'src/index.ts'),
             name: 'musical-components',
-            // the proper extensions will be added
-            fileName: 'index'
+            fileName: `index`
         },
         rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
-            external: ['vue'],
+            external: ['react', 'react-dom'],
             output: {
-                // Provide global variables to use in the UMD build
-                // for externalized deps
                 globals: {
-                    vue: 'Vue'
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
                 }
             }
         },
